dc32bcaa19fadccf1f0952dc3d6d29c833ab0e0e
Vladimir Ratsev <vratsev@gmail.com>
2017-06-23 17:22:13 +0500

    Impove babel config using env option

--- .rispa/babel-options.js
+++ .rispa/babel-options.js
@@ -1,19 +1,18 @@
-const isProd = process.env.NODE_ENV === 'production'
 const resolve = require.resolve
 
-const productionPlugins = [
-  resolve('babel-plugin-transform-react-constant-elements'),
-  resolve('babel-plugin-transform-react-inline-elements'),
-  resolve('babel-plugin-transform-react-remove-prop-types'),
-]
-
-const developmentPlugins = [
-  resolve('react-hot-loader/babel'),
-]
-
 export default () => ({
-  plugins: [
-    ...(isProd ? productionPlugins : []),
-    ...(!isProd ? developmentPlugins : []),
-  ],
+  env: {
+    production: {
+      plugins: [
+        resolve('babel-plugin-transform-react-constant-elements'),
+        resolve('babel-plugin-transform-react-inline-elements'),
+        resolve('babel-plugin-transform-react-remove-prop-types'),
+      ],
+    },
+    development: {
+      plugins: [
+        resolve('react-hot-loader/babel'),
+      ],
+    },
+  },
 })
