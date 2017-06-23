88ed0f1dd9be1fb764d0873a2c874f5da1d9c730
Vladimir Ratsev <vratsev@gmail.com>
2017-06-23 10:09:15 +0500

    Add react-hot-loader/babel plugin

--- .rispa/babel-options.js
+++ .rispa/babel-options.js
@@ -1,10 +1,19 @@
 const isProd = process.env.NODE_ENV === 'production'
 const resolve = require.resolve
 
+const productionPlugins = [
+  resolve('babel-plugin-transform-react-constant-elements'),
+  resolve('babel-plugin-transform-react-inline-elements'),
+  resolve('babel-plugin-transform-react-remove-prop-types'),
+]
+
+const developmentPlugins = [
+  resolve('react-hot-loader/babel'),
+]
+
 export default () => ({
   plugins: [
-    isProd ? resolve('babel-plugin-transform-react-constant-elements') : null,
-    isProd ? resolve('babel-plugin-transform-react-inline-elements') : null,
-    isProd ? resolve('babel-plugin-transform-react-remove-prop-types') : null,
-  ].filter(Boolean),
+    ...(isProd ? productionPlugins : []),
+    ...(!isProd ? developmentPlugins : []),
+  ],
 })
