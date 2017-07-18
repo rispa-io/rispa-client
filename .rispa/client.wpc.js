0d39d09ef03cf75b96d100d36cbeb60245548b64
Vladimir Ratsev <vratsev@gmail.com>
2017-07-18 14:21:00 +0500

    Setup vendor chunk

--- .rispa/client.wpc.js
+++ .rispa/client.wpc.js
@@ -1,50 +1,89 @@
 import path from 'path'
 import OfflinePlugin from 'offline-plugin'
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
+import { group, env } from '@webpack-blocks/webpack2'
 
-const offline = process.env.NODE_ENV === 'production' && !process.env.DISABLE_OFFLINE
-
-export default context => ({
-  entry: [
-    require.resolve('react-hot-loader/patch'),
-    path.resolve(__dirname, '../src/client.js'),
-  ],
-  resolve: {
-    alias: {
-      react$: require.resolve('react'),
-      'react-dom$': require.resolve('react-dom'),
-    },
-  },
-  plugins: [
-    new context.webpack.DefinePlugin({
-      'process.env.DISABLE_REACT_DEVTOOLS': JSON.stringify(process.env.DISABLE_REACT_DEVTOOLS),
-      'process.env.DISABLE_REDUX_DEVTOOLS': JSON.stringify(process.env.DISABLE_REDUX_DEVTOOLS),
-      'process.env.DISABLE_OFFLINE': JSON.stringify(process.env.DISABLE_OFFLINE),
-    }),
-    new context.webpack.ProvidePlugin({
-      Promise: require.resolve('bluebird'),
-    }),
-    new context.webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks: module =>
-        module.context && (
-          /node_modules(\\|\/)react(\\|\/)lib/.test(module.context) ||
-          /node_modules(\\|\/)react-dom(\\|\/)lib/.test(module.context) ||
-          /node_modules(\\|\/)bluebird/.test(module.context)
-        ),
-    }),
-    process.env.ANALYZE_BUNDLE ? new BundleAnalyzerPlugin({
-      analyzerMode: 'static',
-    }) : null,
-    offline ? new OfflinePlugin({
-      externals: [
-        '/shell',
+export default group([
+  context => ({
+    entry: {
+      main: [
+        path.resolve(__dirname, '../src/client.js'),
       ],
-      ServiceWorker: {
-        events: true,
-        navigateFallbackURL: '/shell',
+    },
+    plugins: [
+      new context.webpack.DefinePlugin({
+        'process.env.DISABLE_REACT_DEVTOOLS': JSON.stringify(process.env.DISABLE_REACT_DEVTOOLS),
+        'process.env.DISABLE_REDUX_DEVTOOLS': JSON.stringify(process.env.DISABLE_REDUX_DEVTOOLS),
+        'process.env.DISABLE_OFFLINE': JSON.stringify(process.env.DISABLE_OFFLINE),
+      }),
+      new context.webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor',
+        chunks: ['main'],
+        minChunks: Infinity,
+      }),
+      process.env.ANALYZE_BUNDLE ? new BundleAnalyzerPlugin({
+        analyzerMode: 'static',
+      }) : null,
+    ].filter(Boolean),
+  }),
+  env('development', [
+    () => ({
+      entry: {
+        vendor: [
+          require.resolve('react-hot-loader/patch'),
+          require.resolve('bluebird'),
+          require.resolve('react'),
+          require.resolve('react-dom'),
+          require.resolve('react-router'),
+          require.resolve('react-router-dom'),
+          require.resolve('prop-types'),
+        ],
       },
-      AppCache: false,
-    }) : null,
-  ].filter(Boolean),
-})
+      resolve: {
+        alias: {
+          react$: require.resolve('react'),
+          'react-dom$': require.resolve('react-dom'),
+          'react-router$': require.resolve('react-router'),
+          'react-router-dom$': require.resolve('react-router-dom'),
+          'prop-types$': require.resolve('prop-types'),
+        },
+      },
+    }),
+  ]),
+  env('production', [
+    () => ({
+      entry: {
+        vendor: [
+          require.resolve('react/dist/react.min.js'),
+          require.resolve('bluebird/js/browser/bluebird.min.js'),
+          require.resolve('react-dom/dist/react-dom.min.js'),
+          require.resolve('react-router/umd/react-router.min.js'),
+          require.resolve('react-router-dom'),
+          require.resolve('prop-types/prop-types.min.js'),
+        ],
+      },
+      resolve: {
+        alias: {
+          react$: require.resolve('react/dist/react.min.js'),
+          'react-dom$': require.resolve('react-dom/dist/react-dom.min.js'),
+          'react-router$': require.resolve('react-router/umd/react-router.min.js'),
+          'react-router-dom$': require.resolve('react-router-dom'),
+          'prop-types$': require.resolve('prop-types/prop-types.min.js'),
+        },
+      },
+      plugins: [
+        !process.env.DISABLE_OFFLINE ? new OfflinePlugin({
+          externals: [
+            '/shell',
+          ],
+          ServiceWorker: {
+            minify: true,
+            events: true,
+            navigateFallbackURL: '/shell',
+          },
+          AppCache: false,
+        }) : null,
+      ].filter(Boolean),
+    }),
+  ]),
+])
---
+++ src/start.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+import {
+  Provider,
+  ConnectedRouter,
+  configureStore,
+  createWhen,
+} from '@rispa/redux'
+import { CookiesProvider, Cookies } from 'react-cookie'
+import createHistory from 'history/createBrowserHistory'
+import routes from '@rispa/routes'
+
+const history = createHistory()
+
+const reduxDevtoolCompose = !process.env.DISABLE_REDUX_DEVTOOLS
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ // eslint-disable-line no-underscore-dangle
+  : null
+
+const store = configureStore(
+  history,
+  window.RISPA_INITIAL_STATE,
+  reduxDevtoolCompose,
+)
+const when = createWhen(store, window.RISPA_INITIAL_STATE)
+const cookies = new Cookies()
+
+const render = getRoutes => {
+  ReactDOM.render(
+    <AppContainer>
+      <Provider store={store}>
+        <CookiesProvider>
+          <ConnectedRouter history={history}>
+            {getRoutes({ store, when, cookies })}
+          </ConnectedRouter>
+        </CookiesProvider>
+      </Provider>
+    </AppContainer>,
+    document.getElementById('root')
+  )
+}
+
+render(routes)
+
+if (process.env.NODE_ENV !== 'production' && module.hot) {
+  module.hot.accept('@rispa/routes', () => {
+    when.clear()
+    const newRoutes = require('@rispa/routes').default // eslint-disable-line global-require
+    render(newRoutes)
+  })
+}
+
+//
+// disable react-devtools for production
+//
+/* eslint-disable no-underscore-dangle */
+if (
+  process.env.DISABLE_REACT_DEVTOOLS &&
+  window.__REACT_DEVTOOLS_GLOBAL_HOOK__ &&
+  Object.keys(window.__REACT_DEVTOOLS_GLOBAL_HOOK__._renderers).length
+) {
+  window.__REACT_DEVTOOLS_GLOBAL_HOOK__._renderers = {}
+}
+
+//
+// offline setup
+//
+if (process.env.NODE_ENV === 'production' && !process.env.DISABLE_OFFLINE) {
+  /* eslint-disable global-require */
+  require('offline-plugin/runtime').install()
+}
